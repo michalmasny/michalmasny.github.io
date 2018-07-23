d4e05c46850f51990e3b31d8cf2ea110fdda9b66
michal <m.s.masny@gmail.com>
2018-07-23 17:50:16 +0200

    22

--- resources/Welcome.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180417
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');applyEffects()}
